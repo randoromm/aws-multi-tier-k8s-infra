657cf8da1ad53ec1773d68669bb25a1463b6b651
Rando R <r.rommot@gmail.com>
2025-01-05 22:19:56 +0200

    Fixed issues with outputs. Edited Readme and .gitignore.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # aws-multi-tier-k8s-infra
 Infrastructure as Code (IaC) setup for a scalable, multi-tier application on AWS using Terraform and Kubernetes (EKS). Includes VPC, RDS PostgreSQL, and Kubernetes manifests for frontend and backend services.
+
+## Modules used:
+https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
+https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
+
--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 # VPC and Networking
 module "vpc" {
   source          = "terraform-aws-modules/vpc/aws"
-  version         = "5.16.0" # updated version
+  version         = "5.16.0" # Updated version
   name            = "devops-vpc"
   cidr            = "10.0.0.0/16"
   azs             = ["eu-west-1a", "eu-west-1b"]
@@ -26,7 +26,7 @@ module "vpc" {
 # EKS cluster
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "19.16.0" # Use the latest module version
+  version         = "20.31.6" # Use the latest module version
   cluster_name    = "devops-eks"
   cluster_version = "1.27" # Kubernetes version
   vpc_id          = module.vpc.vpc_id
@@ -38,7 +38,8 @@ module "eks" {
 }
 
 resource "aws_eks_node_group" "default" {
-  cluster_name    = module.eks.cluster_id
+  cluster_name    = "devops-eks" # module.eks.cluster_id
+  depends_on      = [module.eks] # Explicit dependency on the cluster
   node_group_name = "default"
   node_role_arn   = aws_iam_role.eks_nodes.arn
   subnet_ids      = module.vpc.private_subnets
@@ -71,17 +72,62 @@ resource "aws_iam_role" "eks_nodes" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
-  role       = aws_iam_role.eks_nodes.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+resource "aws_iam_role_policy" "eks_worker_node_policy" {
+  name = "eks-worker-node-policy"
+  role = aws_iam_role.eks_nodes.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "eks:DescribeCluster"
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  role       = aws_iam_role.eks_nodes.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+resource "aws_iam_role_policy" "eks_cni_policy" {
+  name = "eks-cni-policy"
+  role = aws_iam_role.eks_nodes.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:AssignPrivateIpAddresses",
+          "ec2:Describe*",
+          "ec2:UnassignPrivateIpAddresses",
+          "ec2:CreateNetworkInterface",
+          "ec2:DeleteNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DetachNetworkInterface",
+          "ec2:AttachNetworkInterface"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
-  role       = aws_iam_role.eks_nodes.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+resource "aws_iam_role_policy" "cloudwatch_policy" {
+  name = "cloudwatch-agent-policy"
+  role = aws_iam_role.eks_nodes.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:PutLogEvents",
+          "logs:CreateLogStream",
+          "logs:CreateLogGroup",
+          "logs:DescribeLogStreams",
+          "logs:DescribeLogGroups"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
 }
