0a5b2081a41331495075eaf8822bfc254345ee92
Rando R <r.rommot@gmail.com>
2025-01-06 10:28:51 +0200

    RDS security group cidr block added via VPC module.

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"] # Allow access within the VPC
+    cidr_blocks = [module.vpc.vpc_cidr_block] # Allow access within the VPC
   }
 
   egress {
